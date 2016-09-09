ca83425e8fb60f7c9a8f6c5ddb8efed2558f21dd
Rumkin <hello@rumk.in>
2016-09-09 16:32:53 +0000

    utils: update bake file to use new task syntax

--- bake.sh
+++ bake.sh
@@ -1,24 +1,24 @@
-function __init {
-    __initial_deps
+function task:init {
+    task:initial_deps
     npm init
 }
 
-function __initial_deps {
+function task:initial_deps {
     set -e
     bake dev mocha
     bake dev istanbul
 }
 
 # Install node package
-function __i {
+function task:i {
     npm i $@
 }
 
 # Install dev dependency
-function __dev {
+function task:dev {
     npm i --save-dev $@
 }
 
-function __cov {
+function task:cov {
     npm run cov
 }
