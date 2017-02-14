ab320d053757933e40aa03deba54a93b01cf7c98
Rumkin <dev@rumk.in>
2017-02-14 20:42:53 +0300

    [util] update bake file syntax

--- bake.sh
+++ bake.sh
@@ -1,24 +1,24 @@
-function task:init {
+task:init() {
     task:initial_deps
     npm init
 }
 
-function task:initial_deps {
+task:initial_deps() {
     set -e
     bake dev mocha
     bake dev istanbul
 }
 
 # Install node package
-function task:i {
+task:i() {
     npm i $@
 }
 
 # Install dev dependency
-function task:dev {
+task:dev() {
     npm i --save-dev $@
 }
 
-function task:cov {
+task:cov() {
     npm run cov
 }
